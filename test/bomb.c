45b1b8a9f9939b7dcd5946d96c77e35ef2fe9491
itenyh <mkeqi@mkedeMacBook-Pro.local>
2018-02-22 07:56:56 +0800

    gogo

---
+++ test/bomb.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    
+    printf("Welcome to my fiendish little bomb. You have 6 phases with\n");
+    printf("which to blow yourself up. Have a nice day!\n");
+    
+    return 0;
+}
